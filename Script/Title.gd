cbdec5a3a5c0f51ffdd92e9baead83e823c2d04e
makabaka0516 <2769467970@qq.com>
2024-09-13 15:31:06 +0800

    玛卡弱弱地改了一下标题页面

---
+++ Script/Title.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+#这个是实现“进入游戏的函数”
+func _Entering_game() -> void:
+	pass
+#介个是实现“游戏设置”的函数
+func _Game_Setting() -> void:
+	pass
+#介个是实现“读取存档”的函数
+func _Loading_game() -> void:
+	pass
