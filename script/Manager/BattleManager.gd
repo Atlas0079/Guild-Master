7d8b23b9d003d03179fd5302f0230cf338f8c82d
Atlas <2948328662@gmail.com>
2024-10-13 10:28:13 +0800

    更新了脚本10/13

--- script/Manager/BattleManager.gd
+++ script/Manager/BattleManager.gd
@@ -1,86 +1,89 @@
-# 战场管理器-自动加载脚本
-
 extends Node
 
-
-# 定义阵营
 enum Camp {ADVENTURER, MONSTER}
-
-# 定义位置
 enum Position {FRONT_TOP, FRONT_MIDDLE, FRONT_BOTTOM, BACK_TOP, BACK_MIDDLE, BACK_BOTTOM}
 
-# 战场结构
-var battlefield = {
-	Camp.ADVENTURER: {
-		Position.FRONT_TOP: null,
-		Position.FRONT_MIDDLE: null,
-		Position.FRONT_BOTTOM: null,
-		Position.BACK_TOP: null,
-		Position.BACK_MIDDLE: null,
-		Position.BACK_BOTTOM: null
-	},
-	Camp.MONSTER: {
-		Position.FRONT_TOP: null,
-		Position.FRONT_MIDDLE: null,
-		Position.FRONT_BOTTOM: null,
-		Position.BACK_TOP: null,
-		Position.BACK_MIDDLE: null,
-		Position.BACK_BOTTOM: null
-	}
-}
-
-
-
-func initialize_battle(adventurer_team_id: String, monster_team_id: String):
-	# 清空战场
-	clear_battlefield()
+class Battlefield:
+	var id: String
+	var battlefield: Dictionary
 	
-	# 获取队伍数据
+	func _init(_id: String):
+		self.id = _id
+		self.battlefield = {
+			Camp.ADVENTURER: {},
+			Camp.MONSTER: {}
+		}
+		for position in Position.values():
+			self.battlefield[Camp.ADVENTURER][position] = null
+			self.battlefield[Camp.MONSTER][position] = null
+	
+	func clear_battlefield():
+		for camp in battlefield.keys():
+			for position in battlefield[camp].keys():
+				battlefield[camp][position] = null
+	
+	func place_team(team: Dictionary, camp: Camp):
+		for member in team.positions.keys():
+			var position = string_to_position(team.positions[member])
+			if position != null:
+				battlefield[camp][position] = member
+	
+	func string_to_position(pos_string: String) -> int:
+		match pos_string:
+			"FRONT_TOP": return Position.FRONT_TOP
+			"FRONT_MIDDLE": return Position.FRONT_MIDDLE
+			"FRONT_BOTTOM": return Position.FRONT_BOTTOM
+			"BACK_TOP": return Position.BACK_TOP
+			"BACK_MIDDLE": return Position.BACK_MIDDLE
+			"BACK_BOTTOM": return Position.BACK_BOTTOM
+		return -1
+	
+	func display_battlefield():
+		print("战场 %s 状态:" % id)
+		for camp in battlefield.keys():
+			var camp_name = "冒险者" if camp == Camp.ADVENTURER else "怪物"
+			print("  %s阵营:" % camp_name)
+			for position in battlefield[camp].keys():
+				var character = battlefield[camp][position]
+				var pos_name = Position.keys()[position]
+				print("    %s: %s" % [pos_name, character if character else "空"])
+		print("------------------------")
+
+var battlefields = {}
+
+func create_battlefield(battlefield_id: String) -> Battlefield:
+	var new_battlefield = Battlefield.new(battlefield_id)
+	battlefields[battlefield_id] = new_battlefield
+	return new_battlefield
+
+func get_battlefield(battlefield_id: String) -> Battlefield:
+	return battlefields.get(battlefield_id)
+
+func initialize_battle(battlefield_id: String, adventurer_team_id: String, monster_team_id: String):
+	var battlefield = get_battlefield(battlefield_id)
+	if not battlefield:
+		battlefield = create_battlefield(battlefield_id)
+	
+	battlefield.clear_battlefield()
+	
 	var adventurer_team = TeamManager.get_team(adventurer_team_id, false)
 	var monster_team = TeamManager.get_team(monster_team_id, true)
 	
-	# 放置冒险者
-	place_team(adventurer_team, Camp.ADVENTURER)
+	battlefield.place_team(adventurer_team, Camp.ADVENTURER)
+	battlefield.place_team(monster_team, Camp.MONSTER)
 	
-	# 放置怪物
-	place_team(monster_team, Camp.MONSTER)
-	
-	# 显示战场
-	display_battlefield()
+	battlefield.display_battlefield()
 
-func clear_battlefield():
-	for camp in battlefield.keys():
-		for position in battlefield[camp].keys():
-			battlefield[camp][position] = null
-
-func place_team(team: Dictionary, camp: Camp):
-	for member in team.positions.keys():
-		var position = string_to_position(team.positions[member])
-		if position != null:
-			battlefield[camp][position] = member
-
-func string_to_position(pos_string: String) -> int:
-	match pos_string:
-		"FRONT_TOP": return Position.FRONT_TOP
-		"FRONT_MIDDLE": return Position.FRONT_MIDDLE
-		"FRONT_BOTTOM": return Position.FRONT_BOTTOM
-		"BACK_TOP": return Position.BACK_TOP
-		"BACK_MIDDLE": return Position.BACK_MIDDLE
-		"BACK_BOTTOM": return Position.BACK_BOTTOM
-	return -1
-
-func display_battlefield():
-	print("战场状态:")
-	for camp in battlefield.keys():
-		var camp_name = "冒险者" if camp == Camp.ADVENTURER else "怪物"
-		print("  %s阵营:" % camp_name)
-		for position in battlefield[camp].keys():
-			var character = battlefield[camp][position]
-			var pos_name = Position.keys()[position]
-			print("    %s: %s" % [pos_name, character if character else "空"])
-	print("------------------------")
-
-func start_battle(adventurer_team_id: String, monster_team_id: String):
-	initialize_battle(adventurer_team_id, monster_team_id)
-	# 接下来可以添加战斗逻辑
+func start_battle(battlefield_id: String, adventurer_team_id: String, monster_team_id: String):
+	initialize_battle(battlefield_id, adventurer_team_id, monster_team_id)
+	# 这里可以添加更多战斗逻辑
 	pass
+
+func remove_battlefield(battlefield_id: String):
+	battlefields.erase(battlefield_id)
+
+func get_all_battlefield_ids() -> Array:
+	return battlefields.keys()
+
+func get_battlefield_count() -> int:
+	return battlefields.size()
--- script/Manager/NPCManager.gd
+++ script/Manager/NPCManager.gd
@@ -6,11 +6,17 @@ extends Node
 var npc_list: Dictionary = {}
 var gamer_money = 0
 
+const NPC_DATA_PATH = "res://Dataset/NPC/NPCData.json"
+
+func _ready():
+    load_npc_data()
+
 # 创建一个新的NPC
 func create_npc(_npc_name: String, _race: String, _strength: int, _intelligence: int, _dexterity: int, _charisma: int, _constitution: int, _perception: int) -> NPC:
     var new_npc = NPC.new()
     new_npc.init(_npc_name, _race, _strength, _intelligence, _dexterity, _charisma, _constitution, _perception)
     npc_list[_npc_name] = new_npc
+    save_npc_data()
     return new_npc
 
 # 获取NPC
@@ -25,10 +31,12 @@ func update_npc_status(_npc_name: String, _health: int, _mana: int, _stamina: in
         npc.attributes.modify_mana(_mana - npc.attributes.mana)
         npc.attributes.modify_stamina(_stamina - npc.attributes.stamina)
         npc.attributes.modify_stress(_stress - npc.attributes.stress)
+    save_npc_data()
 
 # 删除NPC
 func delete_npc(_npc_name: String):
     npc_list.erase(_npc_name)
+    save_npc_data()
 
 # 更新所有NPC
 func update_all_npcs():
@@ -36,7 +44,54 @@ func update_all_npcs():
         npc.traits_states.update_states()
         npc.traits_states.apply_effects(npc)
         npc.equipment.update_equipment()
+    save_npc_data()
 
 # 获取所有NPC
 func get_all_npcs() -> Array:
     return npc_list.values()
+
+# 保存NPC数据到JSON文件
+func save_npc_data():
+    var file = FileAccess.open(NPC_DATA_PATH, FileAccess.WRITE)
+    if file:
+        var npc_data = {}
+        for npc_name in npc_list:
+            var npc = npc_list[npc_name]
+            npc_data[npc_name] = {
+                "race": npc.attributes.race,
+                "strength": npc.attributes.strength,
+                "intelligence": npc.attributes.intelligence,
+                "dexterity": npc.attributes.dexterity,
+                "charisma": npc.attributes.charisma,
+                "constitution": npc.attributes.constitution,
+                "perception": npc.attributes.perception,
+                "health": npc.attributes.health,
+                "mana": npc.attributes.mana,
+                "stamina": npc.attributes.stamina,
+                "stress": npc.attributes.stress
+            }
+        file.store_string(JSON.stringify(npc_data))
+        file.close()
+    else:
+        print("无法打开文件进行写入：", NPC_DATA_PATH)
+
+# 从JSON文件加载NPC数据
+func load_npc_data():
+    if FileAccess.file_exists(NPC_DATA_PATH):
+        var file = FileAccess.open(NPC_DATA_PATH, FileAccess.READ)
+        if file:
+            var json_string = file.get_as_text()
+            file.close()
+            var json = JSON.new()
+            var error = json.parse(json_string)
+            if error == OK:
+                var npc_data = json.data
+                for npc_name in npc_data:
+                    var data = npc_data[npc_name]
+                    create_npc(npc_name, data["race"], data["strength"], data["intelligence"], 
+                               data["dexterity"], data["charisma"], data["constitution"], data["perception"])
+                    update_npc_status(npc_name, data["health"], data["mana"], data["stamina"], data["stress"])
+            else:
+                print("解析JSON时出错: ", NPC_DATA_PATH)
+    else:
+        print("NPC数据文件不存在: ", NPC_DATA_PATH)
